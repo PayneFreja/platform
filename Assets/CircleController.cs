0d844a935f3c776823ad180bc1438152e5bc0e0a
Freja Payne <Freja.payne@elev.ga.ntig.se>
2023-11-15 09:52:55 +0100

    jkbujv

--- Assets/CircleController.cs
+++ Assets/CircleController.cs
@@ -1,37 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class CircleController : MonoBehaviour
 {
 
     [SerializeField]
-    float speed = 4.1f;
+    float speed = 5;
+
+    [SerializeField]
+    float jumpforce = 100;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    float groundRadius = 0.1f;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    LayerMask groundLayer;
+    bool mayJump = true;
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
-        // float moveY = Input.GetAxisRaw("Vertical");
 
         Vector2 movementX = new Vector2(moveX, 0);
-        // Vector2 movementY = new Vector2(0, moveY);
 
         Vector2 movement = movementX;
 
         transform.Translate(movement * speed * Time.deltaTime);
 
-        if (Mathf.Abs(transform.position.x) > 8.5f)
+        Vector3 size = MakeGroundcheckSize();
+        bool isGrounded = Physics2D.OverlapBox(groundCheck.position, size, 0, groundLayer);
+
+
+        if (Input.GetAxisRaw("Jump") > 0 && mayJump == true && isGrounded == true)
         {
-            transform.Translate(-movementX * speed * Time.deltaTime);
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            Vector2 jump = Vector2.up * jumpforce;
+            rb.AddForce(jump);
+            anim.SetBool("isJumping", false);
+            mayJump = false;
+
+        }
+        if (Input.GetAxisRaw("Jump") == 0)
+        {
+            mayJump = true;
         }
 
-        // if (Mathf.Abs(transform.position.y) > 4.5f)
-        // {
-        //     transform.Translate(-movementY * speed * Time.deltaTime);
-        // }
+        if (rb.velocity.y == 0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", false);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", true);
+        }
 
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+
+        Vector3 size = MakeGroundcheckSize();
+        Gizmos.DrawWireCube(groundCheck.position, size);
+    }
+
+    private Vector3 MakeGroundcheckSize() => new Vector3(1, groundRadius);
+
 }
